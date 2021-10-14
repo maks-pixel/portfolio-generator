ce183d4ff58a850a65732f15cbfc2013467bc7e9
Makda Girmay <makdagirmay4@gmail.com>
2021-10-14 00:29:51 -0400

    creating a prompt for questions and answer

--- app.js
+++ app.js
@@ -17,18 +17,25 @@
 //     });
 // };
 // printProfileData(profileDataArgs);
-const fs = require('fs');
-// const inquirer = require('inquirer');
-const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);//takes in values from the command line
-//minus the 2 first array spots cause they are the path files of node and the current files
-const [name, github] = profileDataArgs;
+//===============================================================================================================
 
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => { //allows you to create an html or other files
+//     if (err) throw new Error(err); // to catch any errors
 
-fs.writeFile('./index.html', generatePage(name, github), err => { //allows you to create an html or other files
-    if (err) throw new Error(err); // to catch any errors
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// })
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
