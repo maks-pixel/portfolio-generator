329c8d075841c53dd3fef9b8c06c18bda7f4f452
Makda Girmay <makdagirmay4@gmail.com>
2021-10-14 00:17:33 -0400

    npms

--- app.js
+++ app.js
@@ -18,16 +18,17 @@
 // };
 // printProfileData(profileDataArgs);
 const fs = require('fs');
+// const inquirer = require('inquirer');
 const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
+const profileDataArgs = process.argv.slice(2);//takes in values from the command line
+//minus the 2 first array spots cause they are the path files of node and the current files
 const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+fs.writeFile('./index.html', generatePage(name, github), err => { //allows you to create an html or other files
+    if (err) throw new Error(err); // to catch any errors
 
     console.log('Portfolio complete! Check out index.html to see the output!');
 })
